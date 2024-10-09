7c53cf319df1d721350d84fa0188ae3d804d93de
Martin Arroyo <ma.arroyoq@duocuc.cl>
2024-10-09 18:47:13 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiRamService } from '../services/api-ram.service';
+import { Info } from '../models/info';
+import { Result } from '../models/result';
+import { AlertController, LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage implements OnInit{
+
+  info!: Info;
+  results!: Result[];
+
+  pageNumber: number = 1;
+
+  constructor(
+    private api: ApiRamService,
+    private alertController: AlertController,
+    private loadingController: LoadingController
+  ) {}
+
+  ngOnInit() {
+    this.loadPage();
+  }
+
+  backPage(){
+    if(this.pageNumber > 1){
+      this.pageNumber--;
+      this.loadPage();
+    }
+  }
+
+  nextPage(){
+    if(this.pageNumber < this.info.pages){
+      this.pageNumber++;
+      this.loadPage();
+    }
+  }
+
+  goToPage(page: number){
+    this.pageNumber = page;
+    this.loadPage();
+  }
+
+  async loadPage() {
+    const loader = await this.loadingController.create({
+      message: 'Cargando...'
+    });
+    loader.present()
+    this.api.getCharacters(this.pageNumber).subscribe((data) => {
+      this.info = data.info;
+      this.results = data.results;
+      console.log(data);
+      loader.dismiss();
+    }, (error) => {
+      console.log(error);
+      loader.dismiss();
+      this.alertController.create({
+        header: 'Error al cargar',
+        message: error,
+      })
+      .then(a => a.present());
+    });
+  }
+}
